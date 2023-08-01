6c89c322e70ce88f75e8277e52aaf91a9c315df9
Ido Frizler <idof@microsoft.com>
2023-08-01 22:03:38 +0300

    Fixes to run properly on Azure

--- app.js
+++ app.js
@@ -3,14 +3,17 @@ const fetch = require('node-fetch');
 const cors = require('cors');
 
 const app = express();
-const port = 3000;
-
 const OPENING_MESSAGE = "You are a helpful assistant that summarizes recent messages on thread in a few bullet points (with line breaks). There are potentially up to three parts to your response - a very concise summary of the messages starting with the prefix 'Summary:', a list of key dates mentioned starting with the prefix 'Key dates:', and a list of action items starting with the prefix 'Action items'.";
 
 require('dotenv').config(); // Load environment variables from .env file
 
 app.use(cors());
 app.use(express.json());
+
+// landing page
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
 
 app.post('/api/getSummary', async (req, res) => {
     console.log('Received request to get summary')
@@ -27,6 +30,8 @@ app.post('/api/getSummary', async (req, res) => {
     res.send(data.choices[0].message);
 });
 
+const port = process.env.PORT || 3000; // use the environment variable PORT or, if nothing is provided, use 3000
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+  console.log(`Example app listening on port ${port}`);
 });
+
